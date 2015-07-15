5ea665d097ba9778e67459ea6eda1e34fa8186a3
Ian Bui <thinkbui@gmail.com>
2015-07-15 00:57:10 -0500

    Added first serial test

--- serial_test.rb
+++ serial_test.rb
@@ -1,1 +1,23 @@
+require 'serialport'
 
+begin
+  parameters = { "baud" => 19200,
+                           "data_bits" => 8,
+                           "stop_bits" => 1,
+                           "parity" => SerialPort::NONE }
+  comm_port = 3
+  serial_port = SerialPort.new(comm_port, parameters)
+
+  serial_port.read_timeout = 1000
+  serial_port.write_timeout = 1000
+
+  cmd = 250.chr
+  cmd += 135.chr
+
+  serial_port.write '\xFA\x87'
+  return_str = serial_port.readline(6)
+  return_str.split("").each { |c| puts c.ord }
+
+rescue
+  puts "Error: #{$!}"
+end
