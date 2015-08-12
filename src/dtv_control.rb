ef9e59973591602acef62cf41deda9875000a0a6
Ian Bui <thinkbui@gmail.com>
2015-08-12 00:32:19 -0500

    Added basic channel changer

---
+++ src/dtv_control.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+require 'serialport'
+require_relative './channel_hex'
+
+module DtvControl
+	extend self
+
+	def open_user_channel(main_number,minor_number=65535)
+		cmd = 250.chr
+		cmd += 166.chr
+		cmd += ChannelHex.int_to_hex(main_number)
+		cmd += ChannelHex.int_to_hex(minor_number)
+
+		begin
+			parameters = { "baud" => 9600,
+							"data_bits" => 8,
+							"stop_bits" => 1,
+							"parity" => SerialPort::NONE }
+			comm_port = 3
+			serial_port = SerialPort.new(comm_port, parameters)
+
+			serial_port.read_timeout = 1000
+			serial_port.write_timeout = 1000
+
+			serial_port.write cmd
+			return_str = serial_port.readline(6)
+			return_str.split("").each { |c| puts c.ord }
+
+		rescue => e
+			puts "ERROR: #{e.message}"
+		end
+	end
+end
---
+++ src/example.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+require 'serialport'
+require_relative './dtv_control'
+
+main_number = 668
+minor_number = 2
+
+if main_number && minor_number.nil?
+	puts "Tuning to channel #{main_number}"
+	DtvControl.open_user_channel(main_number)
+elsif main_number && minor_number
+	puts "Tuning to channel #{main_number}-#{minor_number}"
+	DtvControl.open_user_channel(main_number,minor_number)
+end
