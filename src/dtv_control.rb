ed6a49c45557b51346d531eecf6835359d0b9c4f
Ian Bui <thinkbui@gmail.com>
2015-08-13 01:35:24 -0500

    Added helpers to convert numbers to exactly 2 bytes

--- src/dtv_control.rb
+++ src/dtv_control.rb
@@ -3,10 +3,11 @@ require 'serialport'
 require_relative './channel_hex'
 
 module DtvControl
+
 	extend self
 
 	def open_user_channel(main_number,minor_number=nil)
-		cmd = ChannelHex.int_array_to_hex([250,166,main_number,minor_number || 65535])
+		cmd = ChannelHex.int_array_to_hex([250,166].concat(int_to_two_int_byte_array(main_number)).concat(int_to_two_int_byte_array(minor_number || 65535))
 
 		begin
 			parameters = { "baud" => 9600,
@@ -27,4 +28,19 @@ module DtvControl
 			puts "ERROR: #{e.message}"
 		end
 	end
+
+private
+
+	def int_to_two_int_byte_array(num)
+		return [0,0] if !num.is_a? Integer || num > 65535 || num < 0
+		high_byte = num / 256
+		low_byte = num % 256
+		[high_byte,low_byte]
+	end
+
+	def two_int_byte_array_to_int(byte_array)
+		return 0 if !byte_array.all?{|v| v.is_a? Integer} || byte_array.length != 2
+		(byte_array[0] * 256) + byte_array[1]
+	end
+
 end
